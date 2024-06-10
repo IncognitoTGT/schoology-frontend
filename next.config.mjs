1288694b609c0b8e3e44abfb696f527b4113e067
incognitotgt <tgt@incognitotgt.me>
2024-06-10 15:56:30 -0400

    working upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		dangerouslyAllowSVG: true,
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "*.schoology.com",
-				port: "",
-				pathname: "/**",
-			},
-		],
-	},
-	experimental: {
-		after: true,
-	},
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.schoology.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {
+    after: true,
+    serverActions: {
+      bodySizeLimit: '10mb',
+    }
+  },
 };
 
 export default nextConfig;
